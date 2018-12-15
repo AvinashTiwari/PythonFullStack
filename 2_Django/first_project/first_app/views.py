e4152faafde1b28cce58dd0a764abf4643537d02
avinash\avinash.t <qwe123kids@gmail.om>
2018-12-14 18:18:55 -0700

    django

--- 2_Django/first_project/first_app/views.py
+++ 2_Django/first_project/first_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    mydict = {'insert_content': "Hello I am from first app "}
+    return render(request, 'first_app/index.html', context=mydict)
