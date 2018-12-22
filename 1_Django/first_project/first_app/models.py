4641cce9278dafebe5514f68ad7bf780e2360be2
avinash\avinash.t <qwe123kids@gmail.om>
2018-12-22 14:19:53 -0700

    modifed file

--- 1_Django/first_project/first_app/models.py
+++ 1_Django/first_project/first_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Topic(models.Model):
-    topic_name = models.CharField(max_length=264, unique=True)
+    top_name = models.CharField(max_length=264, unique=True)
 
     def __str__(self):
         return self.top_name
