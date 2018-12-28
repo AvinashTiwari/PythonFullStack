272de4899836f9498e924ec1f0a49a460c5d76ba
avinash\avinash.t <qwe123kids@gmail.om>
2018-12-27 17:31:26 -0700

    new

--- 2_Django/ProTwo/appTwo/views.py
+++ 2_Django/ProTwo/appTwo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from appTwo.models import User
 # Create your views here.
+
+def index(request):
+    return render(request, 'app_two/index.html')
+
+def users(request):
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'users': user_list}
+    return render(reqest, 'app_two/user.html', context=user_dict)
