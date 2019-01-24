e14276460baf0db0b4684ed291b653802597586c
avinash\avinash.t <qwe123kids@gmail.om>
2019-01-23 18:43:53 -0700

    django

--- 6_Advanced_Django_CBV/advcbv/basic_app/views.py
+++ 6_Advanced_Django_CBV/advcbv/basic_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
 # Create your views here
+from . import models
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -9,3 +10,37 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['injectme'] = 'BASIC INJECTION'
         return context
+
+class SchoolListView(ListView):
+    # If you don't pass in this attribute,
+    # Django will auto create a context name
+    # for you with object_list!
+    # Default would be 'school_list'
+
+    # Example of making your own:
+    # context_object_name = 'schools'
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_details'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ("name","principal","location")
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ("name","principal")
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
+
+
+class CBView(View):
+    def get(self,request):
+        return HttpResponse('Class Based Views are Cool!')
