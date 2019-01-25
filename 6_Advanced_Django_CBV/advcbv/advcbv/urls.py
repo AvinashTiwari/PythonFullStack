85927e0bdcedb11689afd6e402ac5ca7f2fda097
avinash\avinash.t <qwe123kids@gmail.om>
2019-01-25 14:18:37 -0700

    django

--- 6_Advanced_Django_CBV/advcbv/advcbv/urls.py
+++ 6_Advanced_Django_CBV/advcbv/advcbv/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from basic_app import views
-from django.conf.urls import url
+from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls,name='admin'),
